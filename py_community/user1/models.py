9459e7a338449185addca75171f26d310e234c04
HyeokSeong Kwon <khs50851@naver.com>
2020-12-01 20:34:19 +0900

    이메일 필드 추가

--- py_community/user1/models.py
+++ py_community/user1/models.py
@@ -6,6 +6,11 @@ from django.db import models
 class User1(models.Model):
     username = models.CharField(max_length=32,
                                 verbose_name='사용자명')
+    useremail = models.EmailField(max_length=128,  # 이메일필드는 char필드랑 비슷한데 이메일 형식으로 받게함
+                                  verbose_name='이메일')  # 만약에 이메일 필드를 추가할 경우 먼저 데이터베이스에 이메일 필드 없이 데이터가 있을때
+    # 이메일 필드를 추가해서 migrations하면 내가 직접 default값 넣을건지 default='', verbose_name='이메일'
+    # 아니면 지정되 default값 넣을건지 물어봄
+    # 우선 여기서 1번을 눌러 기본값 넣음
     password = models.CharField(max_length=64,
                                 verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(auto_now_add=True,
--- py_community/user1/views.py
+++ py_community/user1/views.py
@@ -9,7 +9,7 @@ def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
 
-      # 템플릿 폴더를 바라보고 있기 때문에 바로 찾으낼 수 있음
+    # 템플릿 폴더를 바라보고 있기 때문에 바로 찾으낼 수 있음
     # 템플릿 안에 다른 폴더가 있으면 folder/folder2/register.html 이런식으로 하면 됨
     # 다음은 py_community urls로 가서 url 설정
     elif request.method == 'POST':
@@ -17,17 +17,19 @@ def register(request):
         username = request.POST.get('username', None)
         # 이렇게 폼데이터로 넘어오는건 딕셔너리 형태로 되어있음 request.POST['username'] 원랜 이런식
         # 키에 대한 값을 가져오는데 없으면 none으로
+        useremail = request.POST.get('useremail', None)
         password = request.POST.get('password', None)
         re_password = request.POST.get('re_password', None)
 
         res_data = {}
-        if not (username and password and re_password):
+        if not (username and useremail and password and re_password):
             res_data['error'] = '모든 값을 입력해야합니다.'
         elif password != re_password:
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
             user1 = User1(
                 username=username,
+                useremail=useremail,
                 password=make_password(password)
             )
             user1.save()  # 저장
