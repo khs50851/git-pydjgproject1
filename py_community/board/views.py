a0805f5da3d436ac82d52ae607f7ad53d98b0aba
HyeokSeong Kwon <khs50851@naver.com>
2020-12-03 14:53:07 +0900

    페이징처리

--- py_community/board/views.py
+++ py_community/board/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .models import Board
 from .forms import BoardForm
 from user1.models import User1
@@ -41,8 +42,12 @@ def board_write(request):
 
 def board_list(request):
     # 모든 게시글을 가지고 오면서 정렬도 가능 id의 역순 대쉬 -를 쓰면 거꾸로 가져옴
-    boards = Board.objects.all().order_by('-id')
-    print("type : ", type(boards))
-    print("boards : ", boards.values_list())
+    all_boards = Board.objects.all().order_by('-id')
+    page = int(request.GET.get('p', 1))  # 페이지 값은 p로 가져오고 없으면 default값 1
+    # all_boards(전체 오브젝트)에서 한번에 5개씩 글을 가져옴
+    paginator = Paginator(all_boards, 5)
+    # 원래은 all_boards를 쿼리셋형태로 가져오는데 paginator로
+    # 바꿔서 페이지에 대한 정보를 가지고 있음
+    boards = paginator.get_page(page)
 
     return render(request, 'board_list.html', {'boards': boards})
